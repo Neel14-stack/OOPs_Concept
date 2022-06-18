7bd687b864eda6c683443416268cc428084ff95d
Neel Madhab <54886608+Neel14-stack@users.noreply.github.com>
2022-06-18 23:47:48 +0530

    oops

---
+++ 01.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include "Hero.cpp"
+using namespace std;
+
+
+
+
+int main()
+{
+    Hero h1;
+    cout<<"size of hero class is " <<sizeof(h1);
+
+
+    return 0;
+}
---
+++ Hero.cpp
@@ -0,0 +1,9 @@
+class Hero{
+    char first_name[20];
+    char middle_name[20];
+    char last_name[30];
+    int age;
+    bool gender;
+
+    
+};
