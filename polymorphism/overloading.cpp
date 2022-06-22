348057bb73ebfa84e6a4e0cbee8f97f44b9f825e
Neel Madhab <54886608+Neel14-stack@users.noreply.github.com>
2022-06-22 18:34:21 +0530

    overloading

---
+++ polymorphism/overloading.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+class a{
+
+    public:
+    void data()
+    {
+        cout<<"this is an empty function: "<<endl;
+    }
+
+    void data(int a){
+        cout<<"this function will show a data: "<<a<<endl;
+    }
+
+    void data(int a, int b)
+    {
+        cout<<"this function will show u a 2 numbers: "<<a<<"   "<<b<<endl;
+
+    }
+    char data(char a)
+    {
+        cout<<"char data is showing: "<<a<<endl;
+    }
+};
+
+int main()
+{
+    a ob;
+    ob.data();
+    ob.data(5);
+    ob.data('a');
+    ob.data(5,10);
+
+
+    return 0;
+
+}
